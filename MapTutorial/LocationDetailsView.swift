c9a1776d2f8e52eca07f87f8b29a533ce3c85b59
mesutas <developeras@icloud.com>
2024-01-13 11:59:28 +0300

    1.0.1
    final

---
+++ MapTutorial/LocationDetailsView.swift
@@ -0,0 +1,109 @@
+//
+//  LocationDetailsView.swift
+//  MapTutorial
+//
+//  Created by asdirector on 12.01.2024.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationDetailsView: View {
+    @Binding var mapSelection: MKMapItem?
+    @Binding var show: Bool
+    @State private var lookAroundsScene: MKLookAroundScene?
+    @Binding var getDirections: Bool
+    
+    var body: some View {
+        VStack{
+            HStack{
+                VStack(alignment: .leading){
+                    Text(mapSelection?.placemark.name ?? "")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    Text(mapSelection?.placemark.title ?? "")
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                        .lineLimit(2)
+                        .padding(.trailing)
+                    
+                }
+                Spacer()
+                Button {
+                    show.toggle()
+                    mapSelection = nil
+                }label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(.gray, Color(.systemGray6))
+                }
+            }
+            .padding(.horizontal)
+            .padding(.top)
+            
+            if let scene = lookAroundsScene {
+                LookAroundPreview(initialScene: scene)
+                    .frame(height: 200)
+                    .cornerRadius(12)
+                    .padding()
+            } else {
+                ContentUnavailableView("No preview available", systemImage: "eye.slash")
+            }
+            
+            HStack(spacing: 24){
+                Button {
+                    if let mapSelection{
+                        mapSelection.openInMaps()
+                    }
+                }label: {
+                    Text("Open in Maps")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 170, height: 48)
+                        .background(.green)
+                        .cornerRadius(12)
+                }
+                Button {
+                    getDirections = true
+                    show = false
+                }label: {
+                    Text("Get Directions")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 170, height: 48)
+                        .background(.blue)
+                        .cornerRadius(12)
+                }
+            }
+            .padding(.horizontal)
+            
+        }
+        .onAppear{
+            print("DEBUG: Did call on appear")
+            fetchLookAroundPreview()
+        }
+        .onChange(of: mapSelection){ oldValue, newValue in
+            print("DEBUG: Did call on change")
+            fetchLookAroundPreview()
+        }
+        .padding()
+        
+    }
+}
+
+extension LocationDetailsView{
+    func fetchLookAroundPreview() {
+        if let mapSelection{
+            lookAroundsScene = nil
+            Task {
+                let request = MKLookAroundSceneRequest(mapItem: mapSelection)
+                lookAroundsScene = try? await request.scene
+            }
+        }
+    }
+}
+
+#Preview {
+    LocationDetailsView(mapSelection: .constant(nil), show: .constant(false),getDirections: .constant(false))
+}
---
+++ MapTutorial/MapTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTutorialApp.swift
+//  MapTutorial
+//
+//  Created by asdirector on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MapTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
